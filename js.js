daf58dd0a609024dcfb58d214817fef7bfbb9154
sizoider <sizoider@hotmail.com>
2016-04-20 14:59:21 +0100

    fixed css

--- js.js
+++ js.js
@@ -1,51 +1,53 @@
 
+	var app = {};
 	document.addEventListener("DOMContentLoaded", function(event) {
-	    document.getElementById('submit').onclick = submit;
+	    document.getElementById('submit').onclick = app.submit;
 	  });
 
-	function submit(){
+	 app.submit = function (){
 
 		// Storing user input
-	  tag = document.getElementById("tag").value;
-	  var editedTag = tag.replace(/\s/g, "%20");
-	  tagUrl = 'http://api.bandsintown.com/artists/' + editedTag + '/events.json?api_version=2.0&app_id=sizoider';
-	  recomendUrl = 'http://api.bandsintown.com/artists/' + editedTag + '/events/recommended?location=use_geoip&radius=50&only_recs=true&app_id=sizoider&api_version=2.0&format=json';
+	  app.tag = document.getElementById("tag").value;
+	  var editedTag = app.tag.replace(/\s/g, "%20");
+	  
+	  app.tagUrl = 'http://api.bandsintown.com/artists/' + editedTag + '/events.json?api_version=2.0&app_id=sizoider';
+	  app.recomendUrl = 'http://api.bandsintown.com/artists/' + editedTag + '/events/recommended?location=use_geoip&radius=50&only_recs=true&app_id=sizoider&api_version=2.0&format=json';
 	  
 	  //clear result 
-	  var results = document.getElementById('results');
-	  results.innerHTML = '';
+	  app.results = document.getElementById('results');
+	  app.results.innerHTML = '';
 	  
 	  //clear recommended 
-	  rec = document.getElementById('recommended');
-	  rec.innerHTML = '';
+	  app.rec = document.getElementById('recommended');
+	  app.rec.innerHTML = '';
 
-	  getBandsInTown ();
+	  app.getBandsInTown ();
 	  
 	} // End of Submit function
 
-	function getBandsInTown (){
+	 app.getBandsInTown = function(){
 		
 		$.ajax({
 		  dataType: "jsonp",
-		  url: tagUrl, 
+		  url: app.tagUrl, 
 		  error: function() {
 	      console.log('error');
 	   		},
-		  success: getBandsInTownComplete,
+		  success: app.getBandsInTownComplete,
 		 }); // end of Ajax
 
 		$.ajax({
 		  dataType: "jsonp",
-		  url: recomendUrl, 
+		  url: app.recomendUrl, 
 		  error: function() {
 	      console.log('error');
 	   		},
-		  success: getRecommendedGigs,
+		  success: app.getRecommendedGigs,
 		 }); // end of Ajax
 	
 	}// end of getBandsInTown
 
-	function getBandsInTownComplete (data){
+	 app.getBandsInTownComplete = function (data){
 
 		var bandNameDiv = document.querySelector('.bandname-inner');
 		$("body").animate({"scrollTop": window.scrollY+200}, 1000);
@@ -54,9 +56,9 @@
 		if(data.length > 0){
 			for(var key in data){
 				var obj = data[key];
-				var dateFormatted = formatDate(obj);
+				var dateFormatted = app.formatDate(obj);
 
-                bandNameDiv.innerHTML = tag;
+                bandNameDiv.innerHTML = app.tag;
                 	//forming result div content
 					id = "<div class='date'>" + dateFormatted.day + "</div><div class='month'>" + dateFormatted.m_names[dateFormatted.month] + "</div><div class='place'>" + obj.venue.name + "<a class='tickets' href='" + obj.ticket_url + "'>tickets</a></div><div class='city'>" + obj.formatted_location + "</div>";
 					
@@ -64,7 +66,7 @@
 					div.className = 'res';
 					div.innerHTML = id;
 
-		  		results.appendChild(div)
+		  		app.results.appendChild(div)
 		  		
 	  		}
 		}else{
@@ -74,7 +76,7 @@
 
 		}
   }
-  function getRecommendedGigs (data){
+  app.getRecommendedGigs = function (data){
   		console.log(data);
 		// working with response object
 		if(data.length > 0){
@@ -83,7 +85,7 @@
 					var text = document.createTextNode("You may like these gigs near you");
 					p.appendChild(text);
 					p.className = 'rec-inner';
-					rec.appendChild(p);
+					app.rec.appendChild(p);
 					for(var key in data){
 						var obj = data[key];
 
@@ -93,7 +95,7 @@
 									console.log(deep[name].name);
 								
 								
-						  		dateFormatted = formatDate(obj);
+						  		dateFormatted = app.formatDate(obj);
 						  		
 								id = "<div class='date'>" + dateFormatted.day + "</div><div class='month'>" + dateFormatted.m_names[dateFormatted.month] + "</div><div class='place'>" + deep[name].name + "<a class='tickets' href='" + obj.ticket_url + "'>tickets</a></div><div class='city'>" + obj.formatted_location + "</div>";
 			
@@ -101,7 +103,7 @@
 								div.className = 'res';
 								div.innerHTML = id;
 
-					  		rec.appendChild(div)
+					  		app.rec.appendChild(div)
 					  		}//for 2 deep end
 				  		}// for 1 deep end
 					},3000)//settiemout end
@@ -109,16 +111,16 @@
 			} //if end
   }
   // format dat function
- function formatDate(obj){
+ app.formatDate = function(obj){
 		var date = new Date(obj.datetime);
 		var day = date.getDate();
 		var month = date.getMonth()+1;
 		var m_names = ["Jan", "Feb", "Mar", "Apr", "May", "June", 
 						       "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-						       return {
-							       	day: day,
-							       	month: month,
-							    	m_names :m_names
+				 return {
+						    day: day,
+							month: month,
+							m_names :m_names
 						       };
  }
  
